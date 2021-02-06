0af63669096aa1eae53e8e440d0110b9e7586cd8
Strixez <strixez33@gmail.com>
2021-02-06 10:57:20 +0300

    Third hometask

--- index.js
+++ index.js
@@ -1,114 +1,66 @@
-// 1. Дан код:
+// 1. С помощью цикла while вывести все простые числа в промежутке от 0 до 100
 
-var a = 1, b = 1, c, d;
-c = ++a; alert(c);           // Даёт 2 т.к. префиксная форма инкремента увеличивает на 1 и возвращает новое значение
-d = b++; alert(d);           // 1, постфиксная форма инкремента увеличивает на 1, но возвращает старое значение
-c = (2+ ++a); alert(c);      // 5 т.к. ++a = 3 и прибавляем 2
-d = (2+ b++); alert(d);      // 4 b++ =2 +2 =4
-alert(a);                    // 3 т.к. приняло новое значение
-alert(b);                    // 3 т.к. приняло новое значение благодаря инкременту
-
-
-// 2. Чему будет равен x в примере ниже?
-
-var a = 2;
-var x = 1 + (a *= 2); // 2*2+1=5
-
-// 3. Объявить две целочисленные переменные a и b и задать им произвольные начальные значения. 
-// Затем написать скрипт, который работает по следующему принципу:
-// если a и b положительные, вывести их разность;
-// если а и b отрицательные, вывести их произведение;
-// если а и b разных знаков, вывести их сумму; ноль можно считать положительным числом.
-
-var a = 1;
-var b = 2;
-
-if (a && b > 0) {
-    alert(a - b);
-} else if (a && b < 0) {
-    alert(a * b);
-} else {
-    alert(a + b)
-}
-
-// 4. Присвоить переменной а значение в промежутке [0..15]. С помощью оператора switch организовать вывод чисел от a до 15.
-
-a = 14
-
-switch (a) {
-    case 1:
-        document.write(1);
-    case 2:
-        document.write(2);
-    case 3:
-        document.write(3);
-    case 4:
-        document.write(4);
-    case 5:
-        document.write(5);
-    case 6:
-        document.write(6);
-    case 7:
-        document.write(7);
-    case 8:
-        document.write(8);
-    case 9:
-        document.write(9);
-    case 10:
-        document.write(10);
-    case 11:
-        document.write(11);
-    case 12:
-        document.write(12);
-    case 13:
-        document.write(13);
-    case 14:
-        document.write(14);
-    case 15:
-        document.write(15);
-        break;
-}
-
-// 5. Реализовать основные 4 арифметические операции в виде функций с двумя параметрами. Обязательно использовать оператор return.
-
-function slozhenie (a , b){
-    c = a + b;
-    return c;
-}
-function delenie (a, b){
-    c = a / b;
-    return c;
-}
-function vichitanie (a, b){
-    c = a - b;
-    return c;
-}
-function umnozhenie (a, b){
-    c = a * b;
-    return c;
-}
-
-// 6. Реализовать функцию с тремя параметрами: function mathOperation(arg1, arg2, operation), где arg1, arg2 – 
-// значения аргументов, operation – строка с названием операции. В зависимости от переданного значения операции выполнить одну
-//  из арифметических операций (использовать функции из пункта 3) и вернуть полученное значение (использовать switch).
-
-function mathOperation(arg1, arg2, operation) {
-    if (operation = '+') {
-        arg1 + arg2 ;
-    } else if (operation = '-'){
-        arg1 - arg2;
-    } else if(operation = '/'){
-        arg1 / arg2;
-    } else if(operation = '*'){
-        arg1 * arg2;
-    } else {
-        console.log('Операция неверная');
+var i = 0;
+while(i<100) {
+    if(i==1 ||i==2 || i==3 ||i==5){
+        document.write(i +'-Простое число','<br>');
+        i++;
+    }
+    else if(i % 2 ==0 || i % 3 == 0 || i % 4 ==0 || i % 5 ==0 || i % 7==0){
+        i++;
+    }
+    else{
+        document.write(i +'-Простое число','<br>');
+        i++;
     }
 }
-mathOperation(vichitanie(3,2), umnozhenie(2,2), '+');
-// 7) *Сравнить null и 0. Попробуйте объяснить результат.
 
-var age = null; // null - значение "Ничего" или "Неизвестно"
+// 2. С этого урока начинаем работать с функционалом интернет-магазина. Предположим, есть сущность корзины. 
+// Нужно реализовать функционал подсчета стоимости корзины в зависимости от находящихся в ней товаров.
+// Товары в корзине хранятся в массиве. Задачи:
+// a) Организовать такой массив для хранения товаров в корзине;
+// b) Организовать функцию countBasketPrice, которая будет считать стоимость корзины.
 
-var number = 0; // 0 - число, а null - 'Незивестная информация'
+var basket = [
+    {
+        product: "phone",
+        price: 500
+    },
+    {
+        product: "notebook",
+        price: 700
+    },
+    {
+        product: "t-shirt",
+        price: 50
+    }
+];
 
+function countBasketPrice(basket) {
+    var BasketPrice = 0;
+    for (var prod of basket){
+        BasketPrice += prod.price;
+    }
+    return BasketPrice;
+}
+
+console.log("Стоимость корзины: " + countBasketPrice(basket) + " долларов");
+
+// 3.*Вывести с помощью цикла for числа от 0 до 9, не используя тело цикла. Выглядеть это должно так:
+// for(…){// здесь пусто}
+
+for(i = 0; i<9; i++, document.write(i));
+
+// 4) *Нарисовать пирамиду с помощью console.log, как показано на рисунке, только у вашей пирамиды должно быть 20 рядов, а не 5:
+// x
+// xx
+// xxx
+// xxxx
+// xxxxx
+var arr = [];
+var j = 0;
+while (j < 20){
+    j++;
+    arr.push('x');
+    console.log(arr);
+}
